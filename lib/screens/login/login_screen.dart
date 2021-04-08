f000f4bca08a2c59bcd0cbcc2c54e2fd716ae4db
Michal Predotka <mivoligo@gmail.com>
2021-04-08 15:52:54 +0100

    Add UI to login screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -11,10 +11,83 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Login screen'),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Fluttergram',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(hintText: 'Email'),
+                        onChanged: print,
+                        validator: (value) {
+                          if (value != null) {
+                            !value.contains('@')
+                                ? 'Please provide valid email'
+                                : null;
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(hintText: 'Password'),
+                        onChanged: print,
+                        validator: (value) {
+                          if (value != null) {
+                            value.length < 6
+                                ? 'Please provide longer password'
+                                : null;
+                          }
+                        },
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print('log in');
+                        },
+                        child: Text('Log in'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print('sign in');
+                        },
+                        child: Text('No account? Sign up'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.grey[200],
+                          onPrimary: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
