81ebb5f747d99e85df52f2e28b51d9f216ca7d16
Michal Predotka <mivoligo@gmail.com>
2021-04-09 14:56:28 +0100

    Use nav cubit to switch between items in nav bar

--- lib/screens/nav/nav_screen.dart
+++ lib/screens/nav/nav_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../enums/enums.dart';
+import 'cubit/bottom_nav_bar_cubit.dart';
 import 'widgets/widgets.dart';
 
 class NavScreen extends StatelessWidget {
@@ -9,7 +12,10 @@ class NavScreen extends StatelessWidget {
     return PageRouteBuilder(
       settings: const RouteSettings(name: routeName),
       transitionDuration: Duration(seconds: 0),
-      pageBuilder: (_, __, ___) => NavScreen(),
+      pageBuilder: (_, __, ___) => BlocProvider<BottomNavBarCubit>(
+        create: (context) => BottomNavBarCubit(),
+        child: NavScreen(),
+      ),
     );
   }
 
@@ -25,13 +31,22 @@ class NavScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: Scaffold(
-        body: Text('Nav screen'),
-        bottomNavigationBar: BottomNavBar(
-          items: items,
-          selectedItem: BottomNavItem.feed,
-          onTap: (index) {},
-        ),
+      child: BlocBuilder<BottomNavBarCubit, BottomNavBarState>(
+        builder: (context, state) {
+          return Scaffold(
+            body: Text('Nav screen'),
+            bottomNavigationBar: BottomNavBar(
+              items: items,
+              selectedItem: state.selectedItem,
+              onTap: (index) {
+                final selectedItem = BottomNavItem.values[index];
+                context
+                    .read<BottomNavBarCubit>()
+                    .updateSelectedItem(selectedItem);
+              },
+            ),
+          );
+        },
       ),
     );
   }
