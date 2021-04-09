1bbaf795139d3701f21f7e9e1fcf57040468404e
Michal Predotka <mivoligo@gmail.com>
2021-04-09 21:06:09 +0100

    Add user repository

---
+++ lib/repositories/user/base_user_repository.dart
@@ -0,0 +1,6 @@
+import '../../models/models.dart';
+
+abstract class BaseUserRepository {
+  Future<User> getUserWithId({required String userId});
+  Future<void> updateUser({required User user});
+}
---
+++ lib/repositories/user/user_repository.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../config/paths.dart';
+import '../../models/models.dart';
+import '../repositories.dart';
+
+class UserRepository extends BaseUserRepository {
+  UserRepository({FirebaseFirestore? firebaseFirestore})
+      : _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance;
+
+  final FirebaseFirestore _firebaseFirestore;
+
+  @override
+  Future<User> getUserWithId({required String userId}) async {
+    final doc =
+        await _firebaseFirestore.collection(Paths.users).doc(userId).get();
+    return doc.exists ? User.fromDocument(doc) : User.empty;
+  }
+
+  @override
+  Future<void> updateUser({required User user}) async {
+    await _firebaseFirestore
+        .collection(Paths.users)
+        .doc(user.id)
+        .update(user.toDocument());
+  }
+}
