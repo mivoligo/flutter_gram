17ae3a04f227f2161678f1131b6712e06ff15f05
Michal Predotka <mivoligo@gmail.com>
2021-04-10 12:41:13 +0100

    Provide profile bloc to profile screen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../widgets/error_dialog.dart';
+import 'bloc/profile_bloc.dart';
 
 class ProfileScreen extends StatelessWidget {
   static const String routeName = '/profile';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('profile'),
-      ),
+    return BlocConsumer<ProfileBloc, ProfileState>(
+      listener: (context, state) {
+        if (state.status == ProfileStatus.error) {
+          showDialog(
+            context: context,
+            builder: (context) => ErrorDialog(content: state.failure.message!),
+          );
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          body: Center(
+            child: Text('profile'),
+          ),
+        );
+      },
     );
   }
 }
