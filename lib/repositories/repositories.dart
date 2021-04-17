5bb37fb471cc51d348387810f991331cb5c33d2f
Michal Predotka <mivoligo@gmail.com>
2021-04-17 21:36:08 +0100

    Add storage repository

--- lib/repositories/repositories.dart
+++ lib/repositories/repositories.dart
@@ -1,4 +1,6 @@
 export 'auth/auth_repository.dart';
 export 'auth/base_auth_repository.dart';
+export 'storage/base_storage_repository.dart';
+export 'storage/storage_repository.dart';
 export 'user/base_user_repository.dart';
 export 'user/user_repository.dart';
