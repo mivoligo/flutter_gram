176ec783d7c609dc545dde68edd9afd193ff4296
Michal Predotka <mivoligo@gmail.com>
2021-04-27 13:27:19 +0100

    Add function to search users

--- lib/repositories/user/base_user_repository.dart
+++ lib/repositories/user/base_user_repository.dart
@@ -3,4 +3,5 @@ import '../../models/models.dart';
 abstract class BaseUserRepository {
   Future<User> getUserWithId({required String userId});
   Future<void> updateUser({required User user});
+  Future<List<User>> searchUsers({required String query});
 }
