5ab226da2474d7d51c61b2589a44e85d9bc3fab5
Michal Predotka <mivoligo@gmail.com>
2021-05-10 17:19:13 +0100

    Add UI to fetch user feed

--- lib/screens/feed/feed_screen.dart
+++ lib/screens/feed/feed_screen.dart
@@ -1,24 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../widgets/widgets.dart';
 
-class FeedScreen extends StatelessWidget {
+import 'bloc/feed_bloc.dart';
+
+class FeedScreen extends StatefulWidget {
   static const String routeName = '/feed';
 
   @override
+  _FeedScreenState createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: TextButton(
-            onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) {
-                    return Scaffold(
-                      appBar: AppBar(
-                        title: Text('hello'),
-                      ),
-                    );
-                  },
-                )),
-            child: Text('feed')),
-      ),
+    return BlocConsumer<FeedBloc, FeedState>(
+      listener: (context, state) {
+        if (state.status == FeedStatus.error) {
+          showDialog(
+            context: context,
+            builder: (context) => ErrorDialog(
+              content: state.failure.message ?? 'this is error',
+            ),
+          );
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Fluttergram'),
+            actions: [
+              if (state.posts.isEmpty && state.status == FeedStatus.loaded)
+                IconButton(
+                  onPressed: () =>
+                      context.read<FeedBloc>().add(FeedFetchPosts()),
+                  icon: const Icon(Icons.refresh),
+                )
+            ],
+          ),
+          body: _buildBody(state),
+        );
+      },
     );
+  }
+
+  Widget _buildBody(FeedState state) {
+    switch (state.status) {
+      case FeedStatus.loading:
+        return Center(child: CircularProgressIndicator());
+      default:
+        return RefreshIndicator(
+          onRefresh: () async {
+            context.read<FeedBloc>().add(FeedFetchPosts());
+          },
+          child: ListView.builder(
+            itemCount: state.posts.length,
+            itemBuilder: (context, index) {
+              final post = state.posts[index];
+              return PostView(post: post!, isLiked: false);
+            },
+          ),
+        );
+    }
   }
 }
